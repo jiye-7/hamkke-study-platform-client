60c8f303ba5020db60400671c9691136ebf72f9a
jiye-7 <62678492+jiye-7@users.noreply.github.com>
2022-02-18 18:57:40 +0900

    Revert "Issue #14 refactoring register validate"

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import Navbar from './components/views/Navbar/Navbar';
-import WritePage from './components/views/WritePage/WritePage';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <div>
         <Routes>
           <Route path='/' element={<LandingPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/write' element={<WritePage />} />
         </Routes>
       </div>
     </Router>
--- src/components/views/WritePage/WritePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function WritePage() {
-  return <div>Write Page</div>;
-}
-
-export default WritePage;

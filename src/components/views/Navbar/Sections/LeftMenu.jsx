8625358239d7ee0f59e30d03fa1d7f2cef5793d0
jiye-7 <62678492+jiye-7@users.noreply.github.com>
2022-03-03 23:01:43 +0900

    fix: navbar 수정 (#31)

--- src/components/views/Navbar/Sections/LeftMenu.jsx
+++ src/components/views/Navbar/Sections/LeftMenu.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HamkkeLogo from '../../../utils/image/hamkkelogo.jpg';
-import '../Navbar.scss';
 
 function LeftMenu() {
 	return (

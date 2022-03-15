417958661d2c91f1798e1c39a82a8fe81517d4ae
jiye <jy522hi@gmail.com>
2022-03-16 02:18:03 +0900

    loading component 추가

--- src/components/views/LandingPage/LandingPage.jsx
+++ src/components/views/LandingPage/LandingPage.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getPosts } from '../../../_actions/postAction';
 import { useInView } from 'react-intersection-observer';
+import { getPosts } from '../../../_actions/postAction';
 import Post from '../Post/Post';
+import Loading from '../../utils/Loading/Loading';
 
 const LandingPage = () => {
 	const dispatch = useDispatch();
 	const { posts } = useSelector(({ post }) => post);
 
+	const [isLoaded, setIsLoaded] = useState(false);
+
 	const { ref, inView, entry } = useInView({
 		/* Optional options */
 		threshold: 0,
 	});
-
-	console.log(inView, entry);
-
 	useEffect(() => {
 		dispatch(getPosts());
 	}, []);
@@ -29,6 +29,7 @@ const LandingPage = () => {
 			{/* TODO: language filter section */}
 			{/* TODO: filter section(recent, like, recruiting) */}
 			{/* posts section */}
+			{/* <div ref={setTarget}>{isLoaded && <Loading />}</div> */}
 			<section className='post-section'>{renderPost()}</section>
 		</div>
 	);

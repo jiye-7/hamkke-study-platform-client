eb23df6703f47062297f06dbfb925621b39750e4
jiye <jy522hi@gmail.com>
2022-03-16 00:37:08 +0900

    react-intersection-observer module 추가

--- src/components/views/LandingPage/LandingPage.jsx
+++ src/components/views/LandingPage/LandingPage.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPosts } from '../../../_actions/postAction';
+import { useInView } from 'react-intersection-observer';
 import Post from '../Post/Post';
 
 const LandingPage = () => {
 	const dispatch = useDispatch();
 	const { posts } = useSelector(({ post }) => post);
+
+	const { ref, inView, entry } = useInView({
+		/* Optional options */
+		threshold: 0,
+	});
+
+	console.log(inView, entry);
 
 	useEffect(() => {
 		dispatch(getPosts());

0f715804f7150936ea406d0cf7ab3d2b9e0c1fbf
jiye-7 <jy522hi@gmail.com>
2022-03-17 18:48:44 +0900

    issue-#49, issue-#50 게시글 불러오는 로직 수정
    1. stack 선택 시, useEffect가 실행되고, 기존 글이 사라지지 않고, 글이 추가되는 이슈
    2. 다른 페이지 갔다가 돌아왔을 때 기존 글이 사라지지 않고, 글이 추가되는 이슈

--- src/components/views/LandingPage/LandingPage.jsx
+++ src/components/views/LandingPage/LandingPage.jsx
@@ -3,35 +3,47 @@ import { useSelector, useDispatch } from 'react-redux';
 import queryString from 'query-string';
 import Post from '../Post/Post';
 import StackPage from '../StackPage/StackPage';
-import { getPosts } from '../../../_actions/postAction';
+import { getPosts, clearPosts } from '../../../_actions/postAction';
 
 const LandingPage = () => {
 	const dispatch = useDispatch();
 	const { posts, isLastPost } = useSelector(({ post }) => post);
 	const [selectStack, setSelectStack] = useState([]);
-	const [isMoreBtn, setIsMoreBtn] = useState(false);
 	const [page, setPage] = useState(1);
+	const [isMoreBtn, setIsMoreBtn] = useState(false);
+	const [updateState, setUpdateState] = useState('stackUpdate');
 
-	/** 게시글 가져오는 useEffect */
 	useEffect(() => {
 		const query = queryString.stringify(
 			{ stacks: selectStack, limit: 6, page },
 			{ arrayFormat: 'bracket' },
 		);
-		dispatch(getPosts(query));
-		setIsMoreBtn(false);
-	}, [selectStack, page]);
+
+		if (updateState === 'stackUpdate') {
+			(async () => {
+				await dispatch(clearPosts());
+				await dispatch(getPosts(query));
+			})();
+		} else {
+			dispatch(getPosts(query));
+		}
+	}, [selectStack, page, updateState]);
+
+	useEffect(() => {
+	}, []);
 
 	const renderPost = () => {
 		return posts.map((post) => <Post key={post.id} post={post} />);
 	};
 
 	const handleStackCheck = (checkStack) => {
+		setPage(1);
+		setUpdateState('stackUpdate');
 		const stack = checkStack.toLowerCase();
 		const findIdx = selectStack.findIndex((el) => el === stack);
 		const stacks = [...selectStack];
 
-		// findIdx가 -1이면 존재하지 않으니 selectStack에 해당 stack을 추가, findIdx가 0이면 존재하는 경우, selectStack에서 지운다.
+		// findIdx가 -1이면 존재하지 않으니 selectStack에 해당 stack을 추가, findIdx가 존재(0)하는 경우, selectStack에서 지운다.
 		findIdx === -1 ? stacks.push(stack) : stacks.splice(findIdx, 1);
 		setSelectStack(stacks);
 	};
@@ -40,19 +52,18 @@ const LandingPage = () => {
 	const handleIsMore = () => {
 		let nextPage = page + 1;
 		setPage(nextPage);
+		setUpdateState('morePosts');
 		setIsMoreBtn(true);
 	};
 
 	return (
 		<div className='main-container'>
-			{/* language filter section */}
 			<section className='stackFilter-section'>
 				<StackPage stackCheck={handleStackCheck} />
 			</section>
 			{/* TODO: filter section(recent, like, recruiting) */}
-			{/* posts section */}
 			<section className='post-section'>{renderPost()}</section>
-			<section>
+			<section className='more-post-section'>
 				{isLastPost === false && (
 					<button onClick={handleIsMore}>더 보기</button>
 				)}

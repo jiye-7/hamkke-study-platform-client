9f86a70910e405123ee1b2d966d0584bd5bd1de4
jiye-7 <jy522hi@gmail.com>
2022-03-17 16:00:22 +0900

    selectStack state변경하는 코드 변경(indexOf -> findIndex)

--- src/components/views/LandingPage/LandingPage.jsx
+++ src/components/views/LandingPage/LandingPage.jsx
@@ -5,21 +5,12 @@ import Post from '../Post/Post';
 import StackPage from '../StackPage/StackPage';
 import { getPosts } from '../../../_actions/postAction';
 
-const min = 1;
-const max = 10000;
-const randNum = min * Math.random() * (max - min);
-
 const LandingPage = () => {
 	const dispatch = useDispatch();
 	const { posts, isLastPost } = useSelector(({ post }) => post);
 	const [selectStack, setSelectStack] = useState([]);
-	console.log('isLastPost:::::', isLastPost);
-
-	// 무한 스크롤 관련 페이지 정보 state
-	const [page, setPage] = useState(1);
-
-	// 더 보기 버튼 state
 	const [isMoreBtn, setIsMoreBtn] = useState(false);
+	const [page, setPage] = useState(1);
 
 	/** 게시글 가져오는 useEffect */
 	useEffect(() => {
@@ -29,21 +20,18 @@ const LandingPage = () => {
 		);
 		dispatch(getPosts(query));
 		setIsMoreBtn(false);
-	}, [selectStack, page]);
+	}, [selectStack]);
 
-	// 해당 post 1개씩 리턴
 	const renderPost = () => {
 		return posts.map((post) => <Post key={post.id} post={post} />);
 	};
 
 	const handleStackCheck = (checkStack) => {
 		const stack = checkStack.toLowerCase();
-		// selectStack에 stack이 있는 지 확인
-		const findIdx = selectStack.indexOf(stack); // findIndex 사용해보기
+		const findIdx = selectStack.findIndex((el) => el === stack);
 		const stacks = [...selectStack];
 
-		// findIdx가 -1이면 존재하지 않으니 selectStack에 해당 stack을 추가한다.
-		// findIdx가 0이면 존재하는 경우, selectStack에서 지운다.
+		// findIdx가 -1이면 존재하지 않으니 selectStack에 해당 stack을 추가, findIdx가 0이면 존재하는 경우, selectStack에서 지운다.
 		findIdx === -1 ? stacks.push(stack) : stacks.splice(findIdx, 1);
 		setSelectStack(stacks);
 	};

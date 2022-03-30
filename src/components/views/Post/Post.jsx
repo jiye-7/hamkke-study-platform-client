bfd4f78857b129bbe65f5e6a29df4f3765d73bb6
jiye-7 <jy522hi@gmail.com>
2022-03-30 22:21:22 +0900

    fix: 랜딩 페이지에서 각 게시글마다 댓글 개수 보여주기

--- src/components/views/Post/Post.jsx
+++ src/components/views/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PostInfoItem from './PostInfoItem/PostInfoItem';
 import languageOptions from '../../utils/data/language';
--- src/components/views/Post/PostInfoItem/PostInfoItem.jsx
+++ src/components/views/Post/PostInfoItem/PostInfoItem.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { CommentOutlined, EyeOutlined, HeartOutlined } from '@ant-design/icons';
 
 const PostInfoPage = (props) => {
+	const { page, userInfo, handleLikePost, post } = props;
+
 	return (
 		<div className='post-info-container'>
-			{props.page === 'landing' ? (
+			{page === 'landing' ? (
 				<div
 					className={`${
 						props.recruitmentEnd
@@ -14,29 +16,29 @@ const PostInfoPage = (props) => {
 				>
 					<div>
 						<CommentOutlined />
-						<span>100</span>
+						<span>{post.comment}</span>
 					</div>
 					<div>
 						<EyeOutlined />
-						<span>{props.post.hit}</span>
+						<span>{post.hit}</span>
 					</div>
 					<div>
 						<HeartOutlined />
-						<span>{props.post.like}</span>
+						<span>{post.like}</span>
 					</div>
 				</div>
 			) : (
 				<div className='detail-page'>
 					<div>
 						<EyeOutlined />
-						<span>{props.post.hit}</span>
+						<span>{post.hit}</span>
 					</div>
 					<div>
 						<HeartOutlined
-							className={`${props.post.isLike ? 'like' : 'unlike'}`}
-							onClick={props.userInfo.id && props.handleLikePost}
+							className={`${post.isLike ? 'like' : 'unlike'}`}
+							onClick={userInfo.id && handleLikePost}
 						/>
-						<span>{props.post.like}</span>
+						<span>{post.like}</span>
 					</div>
 				</div>
 			)}
